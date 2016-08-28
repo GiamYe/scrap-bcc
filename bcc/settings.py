847bee802a114b95fa09f689ec4c6218a0ded22c
andrew ye <yezhkenan@163.com>
2016-08-28 21:24:59 +0800

    update settings

--- bcc/settings.py
+++ bcc/settings.py
@@ -19,7 +19,8 @@ NEWSPIDER_MODULE = 'bcc.spiders'
 #USER_AGENT = 'bcc (+http://www.yourdomain.com)'
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS=32
+CONCURRENT_REQUESTS = 32
+RETRY_TIMES = 10
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
